5297c2ac4b673207f8a035b447f45d1f39de7430
mmoresun <zzzebra@gmail.com>
2023-01-17 17:23:20 +0300

    test about and projects pages added

---
+++ src/pages/projects.js
@@ -0,0 +1,15 @@
+// Step 1: Import React
+import * as React from 'react'
+
+const Projects = () => {
+  return (
+    <main>
+      <h1>Our Projects</h1>
+      <p>Hi there! Here you can see our projects.</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>Our Projects</title> 
+
+export default Projects
