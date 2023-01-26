84a2e6930d4058103ff3ce6c21353f8da9adb0d3
mmoresun <zzzebra@gmail.com>
2023-01-26 20:45:46 +0300

    P changed to Paragraph, more simple to use

--- src/smalls/Paragraph/index.js
+++ src/smalls/Paragraph/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import StyledParagraph from './styles';
 
+// Paragraph has 5 types:
+// - banner
+// - intro
+// - techno
+// - workflow 
+// - attributes
+//  Don't forget to set the type. If you will forget, the text becomes red (it means error) :)
+
 const Paragraph = (props) => {
     return (
         <StyledParagraph type={props.type}>
